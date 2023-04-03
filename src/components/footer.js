467c6d8b9669a7a1c37b05fbddc9929a35012f30
Minakoss <nontaspapastefanis@gmail.com>
2023-04-03 23:45:27 +0300

    comp

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function footer() {
+  return (
+    <footer>
+      <div className="footer-content">
+        <h6> &copy;MiNakos</h6>
+        <a href="mailto:hege@example.com">hege@example.com</a>
+      </div>
+    </footer>
+  );
+}
+
+export default footer;
--- src/pages/filmBW.js
+++ src/pages/filmBW.js
@@ -1,91 +1,9 @@
 import React from "react";
-
+import Navbar from "@/components/Navbar";
 function filmBW() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <div class="navbar-brand">Analog Life</div>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link " aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Catalogue
-                </a>
-                <ul class="dropdown-menu">
-                  <li>
-                    <a class="dropdown-item" href="/filmtype">
-                      Film Type
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/filmcolor">
-                      Film color
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/filmBW">
-                      Film B&W
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/super8">
-                      Super 8
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/Gallery">
-                      Gallery
-                    </a>
-                  </li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/contact">
-                      Contact
-                    </a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
       <div className="imagebg">
         <div class="container">
           <div class="row">
