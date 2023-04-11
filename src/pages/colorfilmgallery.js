765ec804f13f77e7717eeae4362df19b14d88f27
Minakoss <nontaspapastefanis@gmail.com>
2023-04-11 14:19:07 +0300

    gallery

--- src/pages/colorfilmgallery.js
+++ src/pages/colorfilmgallery.js
@@ -6,7 +6,46 @@ function colorfilmgallery() {
   return (
     <>
       <Navbar />
-      <div className="imagebg"></div>
+      <div className="imagebg">
+        <div className="row">
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft ">
+            <img className="img-thumbnail" src="47050013.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img className="img-thumbnail" src="47050024.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img className="img-thumbnail" src="47060024.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img className="img-thumbnail" src="47070004.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft">
+            <img className="img-thumbnail" src="47070020.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img className="img-thumbnail" src="47080005.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img className="img-thumbnail" src="47080010.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img className="img-thumbnail" src="47080016.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft ">
+            <img className="img-thumbnail" src="47080021.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img className="img-thumbnail" src="75350004.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img className="img-thumbnail" src="75350017.jpg" alt="#" />
+          </div>
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img className="img-thumbnail" src="75350034.jpg" alt="#" />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
--- src/pages/super8gallery.js
+++ src/pages/super8gallery.js
@@ -6,7 +6,46 @@ function super8gallery() {
   return (
     <>
       <Navbar />
-      <div className="imagebg"></div>
+      <div className="imagebg">
+        <div class="row">
+          <div className="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft ">
+            <img class="img-thumbnail" src="47050013.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img class="img-thumbnail" src="47050024.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img class="img-thumbnail" src="47060024.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img class="img-thumbnail" src="47070004.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft">
+            <img class="img-thumbnail" src="47070020.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img class="img-thumbnail" src="47080005.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img class="img-thumbnail" src="47080010.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img class="img-thumbnail" src="47080016.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft ">
+            <img class="img-thumbnail" src="47080021.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInLeft slow">
+            <img class="img-thumbnail" src="75350004.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight ">
+            <img class="img-thumbnail" src="75350017.jpg" alt="#" />
+          </div>
+          <div class="col-lg-3 thumb thumbnail animate__animated animate__slideInRight slow">
+            <img class="img-thumbnail" src="75350034.jpg" alt="#" />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
